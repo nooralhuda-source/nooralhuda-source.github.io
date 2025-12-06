9e432827c02c42f6a620e86a3e548a4c374f047e
nooralhuda-source <alishabirilikeairbus@gmail.com>
2025-12-06 18:58:22 +0000

    11

---
+++ books.js
@@ -0,0 +1,15 @@
+// books.js
+window.BOOKS_CONFIG = [
+  {
+    id: 'al-kafi-1',
+    bookId: 'Al-Kafi-Volume-1',
+    book: 'Al-Kafi',
+    chapters: [
+      { id: 0, categoryId: 0, name: 'Introduction', filename: 'alkafi-v1-ch1.html' },
+      { id: 1, categoryId: 1, name: 'Knowledge', filename: 'hadiths.html' },
+      { id: 2, categoryId: 2, name: 'Intelligence', filename: 'alkafi-v1-ch3.html' },
+      { id: 3, categoryId: 3, name: 'Ignorance', filename: 'alkafi-v1-ch4.html' },
+      { id: 4, categoryId: 4, name: 'Belief', filename: 'alkafi-v1-ch5.html' }
+    ]
+  }
+];
